5e4e59b40ea013cb4ea6fda91a5b6dedc26546dc
小 林 <jevonsflash@hotmail.com>
2023-05-30 20:36:00 +0800

    update

--- HoldDownButtonGroup/Controls/HoldDownButton.xaml.cs
+++ HoldDownButtonGroup/Controls/HoldDownButton.xaml.cs
@@ -1,32 +1,47 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+
 using System.Diagnostics;
+
 
 namespace StickyTab.Controls;
 
 public partial class HoldDownButton : ContentView
 {
+    private IList<IView> appendedViews;
+
+
     public HoldDownButton()
     {
         InitializeComponent();
-        Microsoft.Maui.Handlers.ButtonHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+        appendedViews = new List<IView>();
+
+
+        ViewHandler.ViewMapper.AppendToMapping("MyCustomization", (handler, view) =>
         {
+            if (appendedViews.Count > 0)
+            {
+                return;
+            }
+
 #if WINDOWS
-            var touchRecognizer = new TouchRecognizer(handler.PlatformView);
-            touchRecognizer.OnTouchActionInvoked+=TouchRecognizer_OnTouchActionInvoked;  
+            var touchRecognizer = new TouchRecognizer(handler.PlatformView as Microsoft.UI.Xaml.FrameworkElement);
+            touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
 #endif
 #if ANDROID
-            var touchRecognizer = new TouchRecognizer(handler.PlatformView);
-            touchRecognizer.OnTouchActionInvoked+=TouchRecognizer_OnTouchActionInvoked;
+            var touchRecognizer = new TouchRecognizer(handler.PlatformView as Android.Views.View);
+            touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
 
 #endif
+
 #if IOS
-            var touchRecognizer = new TouchRecognizer(handler.PlatformView);
+            var touchRecognizer = new TouchRecognizer(handler.PlatformView as UIKit.UIView);
             touchRecognizer.OnTouchActionInvoked+=TouchRecognizer_OnTouchActionInvoked;
 
-
-            handler.PlatformView.UserInteractionEnabled = true;
-            handler.PlatformView.AddGestureRecognizer(touchRecognizer);
+            (handler.PlatformView as UIKit.UIView).UserInteractionEnabled = true;
+            (handler.PlatformView as UIKit.UIView).AddGestureRecognizer(touchRecognizer);
 #endif
+            this.appendedViews.Add(view);
 
         });
     }
@@ -35,7 +50,7 @@ public partial class HoldDownButton : ContentView
 
     private void TouchRecognizer_OnTouchActionInvoked(object sender, TouchActionEventArgs e)
     {
-        Debug.WriteLine(e.Type +" is Invoked, position:"+e.Location);
+        Debug.WriteLine(e.Type + " is Invoked, position:" + e.Location);
     }
 
     private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
